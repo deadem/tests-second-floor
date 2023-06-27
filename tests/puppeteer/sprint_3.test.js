c08ea5bec87da7dfaa07db127381ac5580479e55
deadem <deadem@gmail.com>
2023-06-27 22:39:43 +0300

    split to simple tests

--- tests/puppeteer/sprint_3.test.js
+++ tests/puppeteer/sprint_3.test.js
@@ -1,31 +1,39 @@
 const puppeteer = require('puppeteer');
 const assert = require('assert');
 
-describe('sprint 3', () => {
+describe('sprint 3. page /', () => {
   before(async () => {
     browser = await puppeteer.launch({ headless: 'new', });
     page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1024 });
     await page.setDefaultTimeout(1000);
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('body');
   });
 
   after(async () => {
     await browser.close();
   });
 
-  it('open / page', async () => {
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('body');
-
+  it('Form tag presence', async () => {
     const form = await page.waitForSelector('form');
     assert.notEqual(form, null, '<form> tag not found');
+  });
 
+  it('Login field presence', async () => {
+    const form = await page.$('form');
     const login = await form.$('input[name=login]');
     assert.notEqual(login, null, '<input name=login> not found. Login field required');
+  });
 
+  it('Password field presence', async () => {
+    const form = await page.$('form');
     const password = await form.$('input[name=password]');
     assert.notEqual(password, null, '<input name=password> not found. Password field required');
+  });
 
+  it('Login button presence', async () => {
+    const form = await page.$('form');
     const authButton = (
       await form.$$eval([
         '::-p-text(Вход)',
